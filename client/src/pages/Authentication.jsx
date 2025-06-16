03cc8723d124a10b77bb5353ae0beba57bf740ae
MohdSaadMa07 <mohdsaadmarifindia@gmail.com>
2025-06-16 17:04:54 +0400

    Add Authentication layout with styled-components

--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -1,37 +1,58 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import logoImage from "../utils/Images/logo.jpg";
+import AuthImage from "../utils/Images/AuthImage.jpg";
 
 const Container = styled.div`
-    flex:1;
-    height: 100%;
-    display: flex;
-    background: ${({theme}) => theme.bg};
+  flex: 1;
+  height: 100vh;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
 `;
 
-const Left=styled.div`
-    flex: 1;
-    background:blue;
-    @media (max-width: 700px) {
-        display: none;
-} 
-`;
-const Right=styled.div`
-    flex:1;
-    background:red;
-}
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  @media (max-width: 700px) {
+    display: none;
+  }
 `;
 
-const Logo
+const Right = styled.div`
+  flex: 1;
+`;
+
+const Logo = styled.img`
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  top: 40px;
+  left: 40px;
+  z-
+  `;
+
+const Image = styled.img`
+  width: 80%;
+  max-width: 400px;
+  height: auto;
+`;
 
 const Authentication = () => {
   return (
     <Container>
       <Left>
-
+        <Logo src={logoImage} alt="Logo" />
+        <Image src={AuthImage} alt="Authentication Visual" />
       </Left>
-      <Right></Right>
+      <Right>
+        
+      </Right>
     </Container>
-  )
-}
+  );
+};
 
-export default Authentication
+export default Authentication;
